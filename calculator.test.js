d622ebda7db295eb5ddc3f84ab1a649cc8e16a31
Luoxi Pan <l.panpax@gmail.com>
2022-12-28 16:19:51 +1100

    Expand calculator tests to include decimals

--- calculator.test.js
+++ calculator.test.js
@@ -2,18 +2,34 @@ import { calculator } from "./calculator";
 
 const calc = calculator;
 
-test('add', () => {
+test('add ints', () => {
     expect(calc.add(1, 1)).toBe(2);
 });
 
-test('subtract', () => {
+test('add decimals', () => {
+    expect(calc.add(0.2, 0.1)).toBeCloseTo(0.3);
+});
+
+test('subtract ints', () => {
     expect(calc.subtract(6, 3)).toBe(3);
 });
 
-test('divide', () => {
+test('subtract decimals', () => {
+    expect(calc.subtract(0.3, 0.1)).toBeCloseTo(0.2);
+});
+
+test('divide ints', () => {
     expect(calc.divide(4, 2)).toBe(2);
 });
 
-test('multiply', () => {
+test('divide - non-int quotient', () => {
+    expect(calc.divide(3, 2)).toBe(1.5);
+});
+
+test('multiply ints', () => {
     expect(calc.multiply(2, 2)).toBe(4);
+});
+
+test('multiply decimals', () => {
+    expect(calc.multiply(2, 0.33)).toBeCloseTo(0.66);
 });
